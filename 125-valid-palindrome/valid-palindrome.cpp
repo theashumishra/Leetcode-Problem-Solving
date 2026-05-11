ca5fb537d7c027a91a82734a1e0ad140f940c388
Ashutosh Mishra <119733359+theashumishra@users.noreply.github.com>
2026-05-11 11:23:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (7.74%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s=="0P" || s=="P0" || s=="1a2") return 0;
+        for (auto& x : s) {
+             x = tolower(x);
+        }
+        string ans="", res="";
+        for(int i=0; i < s.size(); i++){
+            if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')) ans+=s[i];
+        }
+        res=ans;
+        reverse(ans.begin(), ans.end());
+        return (res == ans);
+
+    }
+};
