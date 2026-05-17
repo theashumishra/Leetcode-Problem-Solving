4e3d6744ab9e2c3771b9d100166f07d4930a6b24
Ashutosh Mishra <119733359+theashumishra@users.noreply.github.com>
2026-05-17 13:44:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (19.63%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    int reverse(long x) {
+    int reverse(int x) {
         long ans=0;
         bool isNeg = 0;
-        if(x<0) {
-            isNeg = 1;
-            x=-x;
-        }
+        // if(x<0) {
+        //     isNeg = 1;
+        //     x=-x;
+        // }
+        // Cpp takes care of negative division by itself
         while(x){
             int dig = x%10;
             ans*=10;
@@ -14,8 +15,8 @@ public:
 
             x/=10;
         }
-        if(ans>INT_MAX) return 0;
-        if(isNeg) return -ans;
+        if(ans>INT_MAX || ans<INT_MIN) return 0;
+        //if(isNeg) return -ans;
         else return ans;
     }
 };
