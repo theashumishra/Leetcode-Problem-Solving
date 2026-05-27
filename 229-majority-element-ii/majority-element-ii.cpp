8c49b176bacad65c82c7c7bc8b0e80f9fca65f08
Ashutosh Mishra <119733359+theashumishra@users.noreply.github.com>
2026-05-27 19:22:55 +0530

    Time: 4 ms (46.23%) | Memory: 23.5 MB (81.84%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,10 +2,39 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-         vector<int> ans;
-         unordered_map<int,int> freq;
-         for(int i=0; i<n; i++) freq[nums[i]]++;
-         for(auto it : freq) if(it.second > n/3) ans.push_back (it.first);
-         return ans;
+        vector<int> ans;
+        int cnt1 = 0, maj1= NULL;
+        int cnt2 = 0;
+        int maj2= NULL;
+        for(int i=0; i<n; i++){
+            if(nums[i]==maj1) cnt1++;
+            else if(nums[i]==maj2) cnt2++;
+            else if(cnt1==0) {
+                maj1=nums[i];
+                cnt1=1;
+            }
+            else if(cnt2==0){
+                maj2=nums[i];
+                cnt2=1;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        //return {maj1,maj2};
+        int freq1=0, freq2=0;
+        for(auto num: nums){
+            if(num==maj1) freq1++;
+            else if(num==maj2) freq2++;
+        }
+        if(freq1>n/3) ans.push_back(maj1);
+        if(freq2>n/3) ans.push_back(maj2);
+
+        //  vector<int> ans;
+        //  unordered_map<int,int> freq;
+        //  for(int i=0; i<n; i++) freq[nums[i]]++;
+        //  for(auto it : freq) if(it.second > n/3) ans.push_back (it.first);
+          return ans;
     }
 };
