4d748a318e6f1bb1642589025b03a44787651585
Ashutosh Mishra <119733359+theashumishra@users.noreply.github.com>
2026-06-05 21:00:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (65.83%) - LeetSync

---
+++ 4321-digit-frequency-score/digit-frequency-score.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        int sum = 0;
+        while(n){
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    }
+};
