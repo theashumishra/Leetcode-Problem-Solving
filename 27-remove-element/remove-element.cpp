f04d5255e852773fcb7f0ae7699bb1753a794764
Ashutosh Mishra <119733359+theashumishra@users.noreply.github.com>
2026-06-26 21:42:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (84.03%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        vector<int> ans;
+        // Normal Brute Force using extra space
+        // vector<int> ans;
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i]!=val) ans.push_back(nums[i]);
+        // }   
+        //  for(int i=0; i<ans.size(); i++){
+        //     nums[i] = ans[i];
+        // } 
+        // return ans.size();
+
+
+        // Actual soln --->
+        // Index is used to track where the element should be inserted and i for iterating the whole array, overwrite the array only first k elements are returned hence after that doesn't matter what is there.
+        int index = 0;
         for(int i=0; i<nums.size(); i++){
-            if(nums[i]!=val) ans.push_back(nums[i]);
-        }   
-         for(int i=0; i<ans.size(); i++){
-            nums[i] = ans[i];
-        } 
-        return ans.size();
+            if(nums[i]!=val){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
     }
 };
