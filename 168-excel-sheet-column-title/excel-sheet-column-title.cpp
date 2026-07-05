4ae6cbead39ba44824c724d3ecb26ffa2603a98f
Ashutosh Mishra <119733359+theashumishra@users.noreply.github.com>
2026-07-05 11:11:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (24.84%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -0,0 +1,17 @@
+// After 26 character it is being repeated
+
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string ans;
+        while(columnNumber>0){
+            columnNumber--;
+            int rem = columnNumber%26;
+            columnNumber/=26;
+            //if(rem == 0) ans+='z';
+             ans+=char(rem+'A');
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
