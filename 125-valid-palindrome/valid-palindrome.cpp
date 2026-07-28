2dba043300dd186df21456dfe729636a979b9f68
Ashutosh Mishra <119733359+theashumishra@users.noreply.github.com>
2026-07-28 17:14:28 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (37.14%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -6,13 +6,30 @@ public:
         for (auto& x : s) {
              x = tolower(x);
         }
-        string ans="", res="";
-        for(int i=0; i < s.size(); i++){
-            if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')) ans+=s[i];
-        }
-        res=ans;
-        reverse(ans.begin(), ans.end());
-        return (res == ans);
+        // string ans="", res="";
+        // for(int i=0; i < s.size(); i++){
+        //     if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')) ans+=s[i];
+        // }
+        // res=ans;
+        // reverse(ans.begin(), ans.end());
+        // return (res == ans);
         // for optimisation purpose two pointers can be used, easy hence bf is also ok.
+        int i=0; 
+        int j = s.size()-1;
+        while(i<j){
+            if(!isalnum(s[i])){
+                i++;
+                continue;
+            }
+            if(!isalnum(s[j])){
+                j--;
+                continue;
+            }
+            if(s[i]!=s[j]) return false;
+            else{
+                i++; j--;
+            } 
+        }
+        return true;
     }
 };
